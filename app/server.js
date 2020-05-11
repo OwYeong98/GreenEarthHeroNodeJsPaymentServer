4240eae619d03410bcf533bd61eaf463a3d348b6
Ow Yeong <mun-ji@hotmail.com>
2020-05-11 23:07:51 +0800

    bug

--- app/server.js
+++ app/server.js
@@ -35,22 +35,6 @@ var log = bole('server')
 
 log.info('server process starting')
 
-
-keytest = {
-    "type": process.env.firebase_type,
-    "project_id": process.env.firebase_project_id,
-    "private_key_id": process.env.firebase_private_key_id,
-    "private_key": process.env.firebase_private_key,
-    "client_email": process.env.firebase_client_email,
-    "client_id": process.env.firebase_client_id,
-    "auth_uri": process.env.firebase_auth_uri,
-    "token_uri": process.env.firebase_token_uri,
-    "auth_provider_x509_cert_url": process.env.firebase_auth_provider_x509_cert_url,
-    "client_x509_cert_url": process.env.firebase_client_x509_cert_url
-    }
-    
-    log.info(keytest)
-
 // Note that there's not much logic in this file.
 // The server should be mostly "glue" code to set things up and
 // then start listening

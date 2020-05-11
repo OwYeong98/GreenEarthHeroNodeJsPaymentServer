2cc3b42cc670024d064844a04987d311d76d59db
Ow Yeong <mun-ji@hotmail.com>
2020-05-11 22:43:18 +0800

    bug

--- app/server.js
+++ app/server.js
@@ -39,7 +39,7 @@ log.info('server process starting')
 // Note that there's not much logic in this file.
 // The server should be mostly "glue" code to set things up and
 // then start listening
-app.listen(config.express.port, config.express.ip, function (error) {
+app.listen(process.env.port, function (error) {
   if (error) {
     log.error('Unable to listen for connections', error)
     process.exit(10)

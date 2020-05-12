8c3e1372ff76ec365c4cf0bd71c745e12aa93e5b
Ow Yeong <mun-ji@hotmail.com>
2020-05-12 18:07:12 +0800

    exclude body parser for webhook

--- app/server.js
+++ app/server.js
@@ -3,6 +3,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use((req, res, next)=> {   
+    var data_stream ='';                 
+   
+    // Readable streams emit 'data' events once a listener is added
+    req.setEncoding('utf-8')            
+    .on('data', function(data) {                         
+      data_stream += data;      
+    })    
+    .on('end', function() {           
+      req.rawBody                                                 
+      req.rawBody = data_stream;   
+      next();
+    }) 
+  });
+
 // var unless = function(path, middleware) {
 //     return function(req, res, next) {
 //         if (path === req.path) {
